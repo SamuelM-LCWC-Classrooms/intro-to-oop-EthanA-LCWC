188397de2465d23080ce51008c07c44f53e8fa7d
EthanA-LCWC <144792227+EthanA-LCWC@users.noreply.github.com>
2024-11-19 14:24:14 +0000

    This didn't take as long as I thought it would

---
+++ pet.py
@@ -0,0 +1,9 @@
+class Pet:
+    def __init__(self, name, species, age):
+        self.name = name
+        self.species = species
+        self.age = age
+    def describe(self):
+        print(f"This is {self.name}, a {self.species} who is {self.age} years old.")
+    def celebrate_birthday(self):
+        print(f"Happy Birthday {self.name}! You are now {self.age} years old!")
